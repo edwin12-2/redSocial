b7ba9eaf562ebb38fc8d93f25fbdf34bf25ca6e9
edwin12-2 <80588520+edwin12-2@users.noreply.github.com>
2024-01-22 16:45:20 -0500

    index

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/redSocialAngular')
+// mongoose.connect('mongodb://localhost/redSocialAngular')
+mongoose.connect('mongodb+srv://dw65restore:Wxg7w9eV3nOepnMT@clusterapp.odlu48y.mongodb.net/foodm?retryWrites=true&w=majority')
 .then(ok => console.log('db conectada'))
 .catch(err => console.log(err));
 
